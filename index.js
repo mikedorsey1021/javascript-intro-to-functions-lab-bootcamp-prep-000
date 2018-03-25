c20ce8bd4018db2f3bfcb8041a2f52519f904364
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-03-25 22:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function logShout (string){
 function logWhisper (string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma (string){
+  if (string === string.toLowerCase()){
+    return "I can\'t hear you!"
+  } elseif {
+    (string === string.toUpperCase()){
+      return "YES INDEED"
+    }
+  } else {
+    (string === "I love you, Grandma"){
+      return "I love you, too"
+    }
+  }
+}
