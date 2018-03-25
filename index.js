4c90385010d7aafc6b265b053e5ef986b6d90eac
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-03-25 22:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma (string){
     (string === string.toUpperCase())
       return "YES INDEED"
     }
-   else
+   
     (string === "I love you, Grandma")
       return "I love you, too"
     }
